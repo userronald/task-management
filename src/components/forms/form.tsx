0d2780a2f0d1962e270b46f84e170268adf2e47c
Sahaya Ronaldo <sahayaronaldo398@gmail.com>
2025-03-17 18:25:49 +0530

    Added Modals in Task

--- src/components/forms/form.tsx
+++ src/components/forms/form.tsx
@@ -52,62 +52,64 @@ const Form =({
       <>
         <form
           onSubmit={handleSubmit}
-          className="max-w-md mx-auto bg-black shadow-md rounded px-8 pt-6 pb-8 mb-4 space-y-4 border-2 border-solid rounded-lg border-gray-500"
+          className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center"
         >
-          <div className="flex flex-col">
-            <label htmlFor="title">Title: </label>
-            <input
-              type="text"
-              id="title"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
-              className="px-3 py-2 bg-black rounded-lg border border-gray-500 focus:border-orange-500 focus:outline-none"
-            />
-          </div>
+          <div className="cursor-pointer rounded-lg bg-mainBackgroundColor border-2 border-columnBackgroundColor ring-orange-500 hover:ring-2 p-6  shadow-lg max-w-sm w-full">
+            <div className="flex flex-col">
+              <label htmlFor="title">Title: </label>
+              <input
+                type="text"
+                id="title"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+                className="px-3 py-2 bg-black rounded-lg border border-gray-500 focus:border-orange-500 focus:outline-none"
+              />
+            </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="description">Description: </label>
-            <textarea
-              name="description"
-              id="description"
-              value={description}
-              onChange={(e) => setDescription(e.target.value)}
-              className="px-3 py-2 bg-black rounded-lg border border-gray-500 focus:border-orange-500 focus:outline-none"
-            ></textarea>
-          </div>
+            <div className="flex flex-col">
+              <label htmlFor="description">Description: </label>
+              <textarea
+                name="description"
+                id="description"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                className="px-3 py-2 h-64 bg-black rounded-lg border border-gray-500 focus:border-orange-500 focus:outline-none"
+              ></textarea>
+            </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="dueDate">Due Date: </label>
-            <input
-              type="date"
-              id="dueDate"
-              value={deadline}
-              onChange={(e) => setDeadline(e.target.value)}
-              className="px-3 py-2 bg-black rounded-lg border border-gray-500 focus:border-orange-500 focus:outline-none"
-            />
-          </div>
+            <div className="flex flex-col">
+              <label htmlFor="dueDate">Due Date: </label>
+              <input
+                type="date"
+                id="dueDate"
+                value={deadline}
+                onChange={(e) => setDeadline(e.target.value)}
+                className="px-3 py-2 bg-black rounded-lg border border-gray-500 focus:border-orange-500 focus:outline-none"
+              />
+            </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="priority">Priority: </label>
-            <select
-              name="priority"
-              id="priority"
-              value={priority}
-              onChange={(e) => setPriority(e.target.value)}
-              className="px-3 py-2 bg-black rounded-lg border border-gray-500 focus:border-orange-500 focus:outline-none"
+            <div className="flex flex-col">
+              <label htmlFor="priority">Priority: </label>
+              <select
+                name="priority"
+                id="priority"
+                value={priority}
+                onChange={(e) => setPriority(e.target.value)}
+                className="px-3 py-2 bg-black rounded-lg border border-gray-500 focus:border-orange-500 focus:outline-none"
+              >
+                <option value="low">Low</option>
+                <option value="medium">Medium</option>
+                <option value="high">High</option>
+              </select>
+            </div>
+
+            <button
+              type="submit"
+              className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
             >
-              <option value="low">Low</option>
-              <option value="medium">Medium</option>
-              <option value="high">High</option>
-            </select>
+              Submit
+            </button>
           </div>
-
-          <button
-            type="submit"
-            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
-          >
-           Submit
-          </button>
         </form>
       </>
     );
